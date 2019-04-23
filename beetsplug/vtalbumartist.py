64ce675201c3405176808d84ef434340c7bcc06c
Rafael Parente <rafaelp.dev@gmail.com>
2019-04-22 23:28:28 -0300

    fixed issues with album matching

--- beetsplug/vtalbumartist.py
+++ beetsplug/vtalbumartist.py
@@ -7,7 +7,7 @@ class VTAlbumArtist(BeetsPlugin):
   def __init__(self):
     super(VTAlbumArtist, self).__init__()
     
-    self.album = None
+    self.is_edited = None
     self.artist = None
     
     self.register_listener('import_task_choice', self._import_after_apply)
@@ -25,21 +25,23 @@ class VTAlbumArtist(BeetsPlugin):
     if task.choice_flag == action.APPLY:
       ui.print_(u'Current Virtual Album Artist:')
       if task.is_album:
-        ui.print_(u'{}'.format(task.cur_artist))
+        ui.print_(u'    {}'.format(task.cur_artist))
       else:
-        ui.print_(u'{}'.format(task.item.albumartist))
+        ui.print_(u'    {}'.format(task.item.albumartist))
         
       sel = ui.input_options( (u'Accept as-is', u'Edit') )
       if sel == u'a':
-        pass
+        self.is_edited = False
       elif sel == u'e':
         self.artist = ui.input_(u'Virtual Album Artist:').strip()
-        self.album = task.cur_album if task.is_album else task.item.album
+        self.is_edited = True
       else:
         assert False
   
   def _before_write(self, item, path, tags):
-    if item.album == self.album:
+    if self.is_edited:
       item['vt_albumartist'] = self.artist
+      tags['vt_albumartist'] = self.artist
     elif 'vt_albumartist' not in tags:
       item['vt_albumartist'] = item.albumartist
+      tags['vt_albumartist'] = item.albumartist
