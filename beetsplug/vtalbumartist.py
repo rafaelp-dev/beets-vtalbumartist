800f096212e88e9c5117206973fd03ab22d369b6
Rafael Parente <rafaelp.dev@gmail.com>
2019-04-24 11:10:52 -0300

    Added command to set/reset already imported items

--- beetsplug/vtalbumartist.py
+++ beetsplug/vtalbumartist.py
@@ -21,6 +21,91 @@ class VTAlbumArtist(BeetsPlugin):
     )
     self.add_media_field('vt_albumartist', field)
   
+  def commands(self):      
+    vtalbumartist_cmd = ui.Subcommand('vtalbumartist',
+                                      help=u'set vt_albumartist field/tag for matching items')
+    vtalbumartist_cmd.parser.add_option(
+      u'-r', u'-R', u'--reset', dest='reset',
+      action='store_true', default=False,
+      help=u'match even those with field/tag already set')
+    vtalbumartist_cmd.parser.add_option(
+      u'-q', u'-Q', u'--quiet', dest='quiet',
+      action='store_true', default=False,
+      help=u'never prompt for input: set field/tag automatically')
+    vtalbumartist_cmd.parser.add_option(
+      u'-s', u'-S', u'--singletons', dest='singletons',
+      action='store_true', default=False,
+      help=u'match only non-album tracks (singletons)')
+    vtalbumartist_cmd.func = self.cmd_func
+    return [vtalbumartist_cmd]
+  
+  def cmd_func(self, lib, opts, args):
+    if opts.singletons:
+      items = lib.items(ui.decargs(args) + [u'singleton:true'])
+      self.singleton_func(items, opts)
+    else:
+      albums = lib.albums(ui.decargs(args))
+      self.album_func(albums, opts)
+  
+  def album_func(self, albums, opts):
+    tagged_item_count = 0
+    tagged_album_count = 0
+    for album in albums:
+      self.albumartist = None
+      self.should_set = opts.reset
+      
+      if not opts.reset or not opts.quiet:
+        for item in album.items():
+          if 'vt_albumartist' in item:
+            if self.albumartist is None:
+              self.albumartist = item['vt_albumartist']
+            elif self.albumartist != item['vt_albumartist']:
+              self.albumartist = ""
+          else:
+            self.should_set = True
+      else:
+        self.albumartist = album.albumartist
+      
+      if not self.should_set:
+        continue
+      
+      if not opts.quiet:
+        fmt = u'    $albumartist - $album'
+        self.process_item(album, fmt)
+      for item in album.items():
+        tagged_item_count += self.try_sync(item)
+      tagged_album_count += 1
+    ui.print_()
+    ui.print_(u'Changed {} item(s) in {} album(s)'.format(tagged_item_count, tagged_album_count))
+  
+  def singleton_func(self, items, opts):
+    tagged_item_count = 0
+    for item in items:
+      if not opts.reset and 'vt_albumartist' in item:
+        continue
+      
+      if opts.reset and opts.quiet:
+        self.albumartist = item.albumartist
+      elif 'vt_albumartist' in item:
+        self.albumartist = item['vt_albumartist']
+      else:
+        self.albumartist = None
+      
+      self.should_set = True
+      
+      if not opts.quiet:
+        fmt = u'    $albumartist - $title'
+        self.process_item(item, fmt)
+      
+      tagged_item_count += self.try_sync(item)
+    ui.print_()
+    ui.print_(u'Changed {} item(s)'.format(tagged_item_count))
+  
+  def process_item(self, item, fmt):
+    ui.print_(u'Tagging:')
+    ui.print_(format(item, fmt))
+    self.set_albumartist(item.albumartist)
+  
   def try_sync(self, item):
     if 'vt_albumartist' in item and item['vt_albumartist'] == self.albumartist:
       return 0
